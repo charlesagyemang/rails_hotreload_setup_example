04b069058e434351811d9bcbb845d673ffb0e708
charlesagyemang <micnkru@gmail.com>
2019-08-04 09:02:26 +0000

    Feat: Pull Up Init blog from scratch

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,9 +1,55 @@
 class PostController < ApplicationController
   def index
-
+    @posts = Post.all.order("created_at DESC")
   end
 
   def new
     @post = Post.new
   end
+
+  def show
+    #code
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    #code
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "You Created A Post"
+    else
+      render :new, alert: "Your Post Did Not Create"
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    #code
+  end
+
+
+  def update
+    #code
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: "Post Was Updated Successfully"
+    else
+      render :edit, alert: "Error: Post Was Not Udated"
+    end
+  end
+
+  def delete
+    #code
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to posts_path, notice: "Deleted Successfully"
+    else
+      render :index, notice: "Deleted Failed"
+    end
+  end
+  private
+    def post_params
+      #code
+      params.require(:post).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'posts',     to: "post#index"
-  get 'posts/new', to: "post#new"
+  get     'posts',          to: "post#index"
+
+  get     'posts/new',      to: "post#new"
+
+  get     'posts/:id/edit', to: "post#edit",    as: :edit_post
+
+  get     'posts/:id',      to: "post#show",    as: :post
+
+  patch   'posts/:id',      to: "post#update"
+
+  delete  'posts/:id',      to: "post#delete",  as: :delete_post
+
+  post    'posts',          to: "post#create"
 end
